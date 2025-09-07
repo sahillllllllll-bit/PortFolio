0c1dc0e73203ae3400718995b27043d87f7b43f9
Sahil Singh <sahilsingh59443@gmail.com>
2025-09-07 12:02:15 +0530

    Update Contact.jsx

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -26,7 +26,7 @@ const handleSubmit = async (e) => {
   setIsSubmitting(true);
 
   try {
-    const response = await fetch("http://localhost:8080/send-email", {
+    const response = await fetch("https://sahil-singh.onrender.com/send-email", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
